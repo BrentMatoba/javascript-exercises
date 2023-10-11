837a1bddd0ccdb2ebd0b9616a44a134e505b9d53
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-10-10 18:53:39 -0700

    start palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+//take word
+//iterate through front and from back.
+i = word[0]
+j = word[word.length-1]
+let front = i
+let back = j
 
+//if space or punctuation, skip
+//if letter, compare the two until front index >= back index
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+
+
+
+
+function isAlphanumeric(string){
+    
+}
